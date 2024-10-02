69362de7b2172a61ca5c0a8034481484e7d62aa9
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2024-10-02 21:34:13 +0200

    git init

---
+++ main.py
@@ -0,0 +1,13 @@
+# pip install -r requirements.txt
+from fastapi import FastAPI
+
+def main(name):
+
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
