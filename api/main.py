1f5b82bd2e9649de660549f090ecaa42c5a4451b
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2024-10-03 03:50:33 +0200

    update: new endpoints added

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import FastAPI
 from api.google_api import GoogleAPI
 
@@ -6,14 +8,20 @@ app = FastAPI()
 # Access Google Sheets API service
 GoogleSheetsService = GoogleAPI.initialize_service()
 
-
 @app.get("/")
+async def get_hello():
+    return {'message': 'hello world!'}
+
+@app.get("/random-{limit}")
+async def get_hello(limit: int):
+    return {'your_random_number': random.randint(0, limit)}
+
+@app.get("/p-f-all")
 async def get_product_feed():
     sheet = GoogleSheetsService.spreadsheets()
     result = sheet.values().get(
         spreadsheetId=GoogleAPI.SPREADSHEET_ID, range=GoogleAPI.RANGE
     ).execute()
-
     values = result.get('values', [])
 
     if not values:
